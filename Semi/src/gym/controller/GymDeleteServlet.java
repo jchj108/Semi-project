edaf3dca74a76411da07c83ba7d16af3011b9f6a
DESKTOP-5QRTDIH\user2 <jchj108@naver.com>
2021-06-30 19:19:17 +0900

    gym으로 옮김

--- Semi/src/gym/controller/GymDeleteServlet.java
+++ Semi/src/gym/controller/GymDeleteServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import board.model.service.BoardService;
 import gym.model.service.GymService;
 
 @WebServlet("/gymDelete.do")
--- Semi/src/gym/model/dao/GymDAO.java
+++ Semi/src/gym/model/dao/GymDAO.java
@@ -13,6 +13,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Properties;
 
+import board.model.vo.PageInfo;
 import gym.model.vo.GFile;
 import gym.model.vo.Gym;
 import page.model.vo.Page;
@@ -523,5 +524,226 @@ public class GymDAO {
 			close(pstmt);
 		}
 		return result;
+	}
+
+	public int insertGym(Connection conn, Gym g) {
+		
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String query = prop.getProperty("insertGym");
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, g.getG_TYPE_NM());
+			pstmt.setString(2, g.getG_GU_NM());
+			pstmt.setString(3, g.getG_NAME());
+			pstmt.setString(4, g.getG_ADDRESS());
+			pstmt.setString(5, g.getG_TEL());
+			pstmt.setString(6, g.getG_BIGO());
+			pstmt.setString(7, g.getG_HOMEPAGE());
+			pstmt.setString(8, g.getG_EDU_YN());
+			pstmt.setString(9, g.getG_IN_OUT());
+			pstmt.setString(10, g.getG_PARKING_LOT());
+			pstmt.setInt(11, g.getG_COVID());
+			
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
+
+	public int insertGFile(Connection conn, ArrayList<GFile> fileList) {
+		
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String query = prop.getProperty("insertGFile");
+		
+		try {
+			for(int i = 0; i < fileList.size(); i++) {
+				GFile f = fileList.get(i);
+				
+				pstmt = conn.prepareStatement(query);
+				pstmt.setString(1, f.getgOriginName());
+				pstmt.setString(2, f.getgChangeName());
+				pstmt.setString(3, f.getgFilePath());
+				pstmt.setInt(4, f.getgFileLv());
+				
+				result += pstmt.executeUpdate();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		return result;
+	}
+
+	public int getGymListCount(Connection conn) {
+		
+		Statement stmt = null;
+		ResultSet rset = null;
+		int listCount = 0;
+		
+		String query = prop.getProperty("gymListCount");
+		
+		try {
+			stmt = conn.createStatement();
+			rset = stmt.executeQuery(query);
+			
+			while(rset.next()) {
+				listCount = rset.getInt(1);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(stmt);
+		}
+		return listCount;
+	}
+	
+	public int getSearchGymListCount(Connection conn, String keyword, String category) {
+		
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		int listCount = 0;
+		String query = null;
+		
+		switch(category) {
+
+	
+		case "이름" : 
+			query = prop.getProperty("getSearchGymListCountByName"); 
+			break;
+		case "시설번호" :
+			query = prop.getProperty("getSearchGymListCountByNo"); 
+			break;
+		case "군/구" :
+			query = prop.getProperty("getSearchGymListCountByGu"); 
+			break;
+		case "타입" :
+			query = prop.getProperty("getSearchGymListCountByType"); 
+			break;
+		}
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, keyword);
+			rset = pstmt.executeQuery();
+			
+			while(rset.next()) {
+				listCount = rset.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return listCount;
+	}
+
+	public ArrayList<Gym> searchGym(Connection conn, String keyword, String category, PageInfo pi) {
+		
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		ArrayList<Gym> list = new ArrayList<Gym>();
+		String query = null;
+
+		switch(category) {
+		
+		case "시설번호" :
+			query = prop.getProperty("searchByNo"); break;
+		case "이름" :
+			query = prop.getProperty("searchByName"); break;
+		case "타입" :
+			query = prop.getProperty("searchByType"); break;
+		case "군/구" :
+			query = prop.getProperty("searchByGu"); break;
+		}
+		
+		int startRow = (pi.getCurrentPage() - 1) * pi.getBoardLimit() + 1;
+		int endRow = startRow + pi.getBoardLimit() - 1;
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, keyword);
+			pstmt.setInt(2, startRow);
+			pstmt.setInt(3, endRow);
+			rset = pstmt.executeQuery();
+			while(rset.next()) {
+				Gym g = new Gym(rset.getInt("g_NO"),
+						rset.getString("g_TYPE_NM"),
+						rset.getString("g_GU_NM"),
+						rset.getString("g_NAME"),
+						rset.getString("g_ADDRESS"),
+						rset.getDouble("g_YCODE"),
+						rset.getDouble("g_XCODE"),
+						rset.getString("g_TEL"),
+						rset.getString("g_EDU_YN"),
+						rset.getString("g_IN_OUT"),
+						rset.getString("G_STATUS").charAt(0),
+						rset.getInt("g_COUNT"),
+						rset.getInt("g_COVID"));
+				list.add(g);
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return list;
+	}
+
+	public ArrayList<Gym> selectGymList(Connection conn, PageInfo pi) {
+		
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		ArrayList<Gym> list = new ArrayList<Gym>();
+		
+		String query = prop.getProperty("selectGymLists");
+		
+		int startRow = (pi.getCurrentPage() - 1) * pi.getBoardLimit() + 1;
+		int endRow = startRow + pi.getBoardLimit() - 1;
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, startRow);
+			pstmt.setInt(2, endRow);
+			rset = pstmt.executeQuery();
+			while(rset.next()) {
+				Gym g = new Gym(rset.getInt("g_NO"),
+						rset.getString("g_TYPE_NM"),
+						rset.getString("g_GU_NM"),
+						rset.getString("g_NAME"),
+						rset.getString("g_ADDRESS"),
+						rset.getDouble("g_YCODE"),
+						rset.getDouble("g_XCODE"),
+						rset.getString("g_TEL"),
+						rset.getString("g_EDU_YN"),
+						rset.getString("g_IN_OUT"),
+						rset.getString("G_STATUS").charAt(0),
+						rset.getInt("g_COUNT"),
+						rset.getInt("g_COVID"));
+				list.add(g);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+
+		return list;
 	}
 }
